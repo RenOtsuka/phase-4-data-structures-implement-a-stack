75f36ec9dec3729f83b7a4fdc56a0b1315655437
Ryan Delos Santos <ryansantos.delossantos121@gmail.com>
2023-01-11 11:08:10 -0600

    commit tree bc98270eff8d18a0acde3229fb22ebba911b347f

--- lib/stack.rb
+++ lib/stack.rb
@@ -1,1 +1,20 @@
 # your code here
+class Stack
+
+  def initialize
+    @stack = []
+  end
+
+  def push(value)
+    @stack[@stack.length()] = value
+  end
+
+  def pop
+    @stack.delete_at(@stack.length() - 1)
+  end
+
+  def peek
+    @stack[@stack.length() - 1]
+  end
+
+end
